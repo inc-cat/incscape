56a894904677ace9db37709f37ead48fa939b53d
Anwar Louis <smmaniac@hotmail.co.uk>
2023-09-19 21:55:51 +0100

    Add albums to archive section

--- src/components/Archive/index.jsx
+++ src/components/Archive/index.jsx
@@ -1,23 +1,119 @@
 import wirfos from './albumCovers/wirfos.jpg';
+import impressions2002 from './albumCovers/impressions2002.jpg';
+import structure from './albumCovers/structure.jpg';
 
 export default function Archive() {
   return (
-    <div class="flex-container">
-      <div>
-        <h1>Watching it rain from outer space</h1>
-      </div>
-      <div>
-        <h2>September 2023</h2>
-      </div>
-      <div>
-        <a href="https://anwarlouis.bandcamp.com/album/watching-it-rain-from-outer-space">
-          <img
-            src={wirfos}
-            alt="Watching it rain from outer space (September 2023)"
-          />
-        </a>
+    <div>
+      <div class="album-container">
         <div>
-          <p>Album text here!</p>
+          <h1>Watching it rain from outer space</h1>
+        </div>
+        <div>
+          <h2>September 2023</h2>
+        </div>
+        <div>
+          <a href="https://anwarlouis.bandcamp.com/album/watching-it-rain-from-outer-space">
+            <img
+              src={wirfos}
+              alt="Watching it rain from outer space (September 2023)"
+            />
+          </a>
+          <div>
+            <p>
+              Actually finished in November 2021. Before I moved to the north of
+              England, every time I would travel to places like Manchester, it'd
+              always be raining. My friend lived in a high rise by a busy road
+              and I'd just watch cars pass by during the evenings when things
+              were quiet. It always felt other wordly and I can't really explain
+              why but it was more of me just feeling like I was watching it rain
+              from outer space where the streets of Manchester were isolated
+              from me and my surroundings.{' '}
+            </p>
+          </div>
+          Art by Kuro:{' '}
+          <a href="https://bsky.app/profile/kurobatte.bsky.social">BlueSky</a> |
+          <a href="https://twitter.com/kurobatte"> Twitter </a>
+        </div>
+      </div>
+
+      <div class="album-container">
+        <div>
+          <h1>Impressions: 2002</h1>
+        </div>
+        <div>
+          <h2>August 2023</h2>
+        </div>
+        <div>
+          <a href="https://anwarlouis.bandcamp.com/album/impressions-2002">
+            <img
+              src={impressions2002}
+              alt="Watching it rain from outer space (September 2023)"
+            />
+          </a>
+          <div>
+            <p>
+              It had been such a long time since I put music out, normally I set
+              a day aside to finalise tracks and add processing to make
+              everything sound as good as it possibly can. However since moving
+              to the north of England I have left all of my equipment in London
+              and until I started this project, I hadn't really made much music
+              at all. I decided that I needed to make music again or I'd just
+              fall out of making music in total. I thought the best idea was to
+              continue with an idea I have had on the backburner for a few years
+              now. I had always wanted to make an "Impressions" series but never
+              really got around to doing it as creativity can be all over the
+              place for me. I can hardly ever just finish a project without
+              going off into many directions and because of this I have many
+              unfinished albums that can progress at the oddest times or just
+              stay as 1 track.
+            </p>
+            <p>
+              For this project, instead of music that I felt was more up to date
+              with the short game ideas I had in my head, i decided to take it
+              back to the hayday of the PS2, around the time Ratchet and Clank
+              came out. A lot of game soundtracks were instrumental in inspiring
+              and shaping this project. After being so long out of making music,
+              this project came together quite quickly and even through simply
+              playing around at time, I feel I made some good work here.{' '}
+            </p>
+          </div>
+          Art by Sontra:{' '}
+          <a href="https://bsky.app/profile/sontra.bsky.social">BlueSky</a> |{' '}
+          <a href="https://twitter.com/sontra_"> Twitter </a>
+        </div>
+      </div>
+
+      <div class="album-container">
+        <div>
+          <h1>Watching it rain from outer space</h1>
+        </div>
+        <div>
+          <h2>September 2023</h2>
+        </div>
+        <div>
+          <a href="https://anwarlouis.bandcamp.com/album/watching-it-rain-from-outer-space">
+            <img
+              src={structure}
+              alt="Watching it rain from outer space (September 2023)"
+            />
+          </a>
+          <div>
+            <p>
+              Looking back on this album, I'm not sure I know much about how I
+              felt making it. This was just me making music about my rush hour
+              commute to work. I'd run from the DLR platforms to the Jubilee
+              line and be sandwiched into a small train from Canning Town to
+              Canada Water and then board an Overground train to get me to work.
+              When I wanted a seat, I'd board a train in the wrong direction and
+              go back 1 stop (West Ham) to make sure I got a seat. I don't have
+              fond memories of rush hour travelling. Each song is a mish mash of
+              my my thoughts about those minutes of my morning.
+            </p>
+          </div>
+          Art by Lovewin:{' '}
+          <a href="https://bsky.app/profile/lovewin.bsky.social">BlueSky</a> |
+          <a href="https://twitter.com/lovewin"> Twitter </a>
         </div>
       </div>
     </div>
