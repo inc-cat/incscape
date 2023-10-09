03c1219ebb51b62e35967a9ae7e85e7db8f0eabc
Anwar Louis <smmaniac@hotmail.co.uk>
2023-10-09 17:39:36 +0100

    Fix individual enlargements

--- src/components/Gallery/ImageEntry.jsx
+++ src/components/Gallery/ImageEntry.jsx
@@ -1,6 +1,16 @@
+import { useState } from 'react';
+import './Gallery.css';
+
 export default function ImageEntry(props) {
+  const [enlarge, setEnlarge] = useState(false);
+
   return (
-    <div>
+    <div
+      class={enlarge ? 'gallery-image selected' : 'gallery-image'}
+      onClick={function () {
+        setEnlarge(!enlarge);
+      }}
+    >
       <img src={props.image} />
       <br></br>
       <b>{props.title}</b>
--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,18 +1,13 @@
-import { useState } from 'react';
-import './Gallery.css';
 import shortlands from './Shortlands.png';
 import ImageEntry from './ImageEntry';
 
 export default function Gallery() {
-  const [enlarge, setEnlarge] = useState(false);
-
   return (
-    <div
-      class={enlarge ? 'gallery-image selected' : 'gallery-image'}
-      onClick={function () {
-        setEnlarge(!enlarge);
-      }}
-    >
+    <div>
+      <ImageEntry image={shortlands} title="Shortlands at 6PM" artist="Kite">
+        <a href="https://twitter.com/Kiteincolours">Twitter</a>
+      </ImageEntry>
+
       <ImageEntry image={shortlands} title="Shortlands at 6PM" artist="Kite">
         <a href="https://twitter.com/Kiteincolours">Twitter</a>
       </ImageEntry>
