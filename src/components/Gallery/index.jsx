b890b3338c1400cbde439001dd1b3a528a3d6382
Anwar Louis <smmaniac@hotmail.co.uk>
2023-10-16 16:49:42 +0100

    Modify art section

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,4 +1,6 @@
 import shortlands from './Shortlands.png';
+import utopia from './Uptopia.png';
+import future from './future.png';
 import ImageEntry from './ImageEntry';
 
 export default function Gallery() {
@@ -8,8 +10,13 @@ export default function Gallery() {
         <a href="https://twitter.com/Kiteincolours">Twitter</a>
       </ImageEntry>
 
-      <ImageEntry image={shortlands} title="Shortlands at 6PM" artist="Kite">
-        <a href="https://twitter.com/Kiteincolours">Twitter</a>
+      <ImageEntry image={utopia} title="Utopia on rails" artist="ChayMyatny">
+        <a href="https://twitter.com/ChayMyatny">Twitter</a>
+      </ImageEntry>
+
+      <ImageEntry image={future} title="Future Zone" artist="iloveui">
+        <a href="https://iloveui.com/">Website</a> |{' '}
+        <a href="https://twitter.com/weloveui">Twitter</a>
       </ImageEntry>
     </div>
   );
