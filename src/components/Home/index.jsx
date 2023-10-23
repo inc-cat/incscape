7110f8bf49d3f4cf418107ab0c9322902364d11b
Anwar Louis <smmaniac@hotmail.co.uk>
2023-10-23 16:53:26 +0100

    Build out home page

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,14 @@
+import './Home.css';
+
+export default function Home() {
+  return (
+    <div style={{ height: '100%' }}>
+      <div class="hero-image">
+        <div class="hero-text">
+          <h1>Anwar Louis</h1>
+          <p>I'd like to think I am somewhat versatile.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
